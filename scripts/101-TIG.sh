65e77b0698017963a95c4e42712e18fe1e4569ba
Lavanya Subbarayalu <lavanya-s@HCL.COM>
2018-01-11 15:58:40 +0530

    baseline

---
+++ scripts/101-TIG.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# Script parameters from arguments
+TomcatManagerIP=$1
+JolokiaServerIP=$2
+RedisIP=$3
+HostIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+
+apt-get update
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible -y
+apt-get update
+apt-get install ansible -y
+apt-get install unzip -y
+
+cd /home/
+wget https://raw.githubusercontent.com/devopszone/TIG/master/Configfiles.zip
+unzip Configfiles.zip -d /home/
+
+
+HOME=/root ansible-playbook /home/Configfiles/ansible/docker_install.yml  --extra-vars "HostIP=$HostIP JolokiaServerIP=$JolokiaServerIP RedisIP=$RedisIP TomcatManagerIP=$TomcatManagerIP" -vvv
+
+
